6b42ffee3030c7f5498eb7433a180884790a7d50
marco-29 <marco@jid.agency>
2024-04-18 13:09:12 -0600

    0.3.28 Boton excel

--- application/views/clientes/suspendidos.php
+++ application/views/clientes/suspendidos.php
@@ -25,7 +25,7 @@
 						<a class="dropdown-item" href="<?php echo site_url('ventas/crear_personalizada') ?>">+ Nueva Venta Personalizada</a>
 					</div>
 				</div>
-				<div class="form-group float-md-right mx-1">
+				<div class="form-group float-md-right">
 					<div id="buttons"></div>
 				</div>
 			</div>
