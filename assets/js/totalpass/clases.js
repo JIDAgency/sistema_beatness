82a863a953360dff6469788c04cd4b7bf577d6fb
Eduardo GQ <lalogqs@gmail.com>
2024-08-22 17:55:39 -0600

    1.7.6 Avance de totalpass

--- assets/js/totalpass/clases.js
+++ assets/js/totalpass/clases.js
@@ -1,111 +1,93 @@
-var table;
+let table;
 
-var actual_url = document.URL;
-var method_call = "";
+const actualUrl = document.URL;
+const methodCall = actualUrl.includes("index") ? "" : "";
 
-if (actual_url.indexOf("index") < 0) {
-    method_call = "";
-}
-
-$.fn.dataTable.ext.errMode = 'throw';
-
-$(document).ready(function () {
+document.addEventListener('DOMContentLoaded', function () {
     table = $('#table').DataTable({
-        "searching": true,
-        "scrollX": true,
-        "deferRender": true,
-        'processing': true,
-        "order": [[2, "desc"]],
-        "lengthMenu": [[25, 50, 100, 250, 500, -1], [25, 50, 100, 250, 500, "Todos"]],
-        "ajax": {
-            "url": method_call + "clases_obtener_activas",
-            "type": 'POST'
+        searching: true,
+        scrollX: true,
+        deferRender: true,
+        processing: true,
+        //order: [[2, "desc"]],
+        lengthMenu: [[25, 50, 100, 250, 500, -1], [25, 50, 100, 250, 500, "Todos"]],
+        ajax: {
+            url: `${methodCall}clases_obtener_activas`,
+            type: 'POST'
         },
-        "columns": [
-            { "data": "opciones" },
-            { "data": "id" },
-            { "data": "totalpass_event_id" },
-            { "data": "totalpass_eventOccurrenceUuid" },
-            { "data": "identificador" },
-            { "data": "disciplinas_nombre" },
-            { "data": "dificultad" },
-            { "data": "fecha" },
-            { "data": "horario" },
-            { "data": "instructores_nombre" },
-            { "data": "sucursales_locacion" },
-            { "data": "cupos" },
+        columns: [
+            { data: "opciones" },
+            { data: "id" },
+            { data: "totalpass_event_id" },
+            { data: "totalpass_eventOccurrenceUuid" },
+            { data: "identificador" },
+            { data: "disciplinas_nombre" },
+            { data: "dificultad" },
+            { data: "fecha" },
+            { data: "horario" },
+            { data: "instructores_nombre" },
+            { data: "sucursales_locacion" },
+            { data: "cupos" }
         ],
-        'language': {
-            "sProcessing": '<i class="fa fa-spinner spinner"></i> Cargando...',
-            "sLengthMenu": "Mostrar _MENU_",
-            "sZeroRecords": "No se encontraron resultados",
-            "sEmptyTable": "Ningún dato disponible en esta tabla =(",
-            "sInfo": "Mostrando del _START_ al _END_ de _TOTAL_",
-            "sInfoEmpty": "Mostrando del 0 al 0 de 0",
-            "sInfoFiltered": "(filtrado _MAX_)",
-            "sInfoPostFix": "",
-            "sSearch": "Buscar:",
-            "sUrl": "",
-            "sInfoThousands": ",",
-            "sLoadingRecords": "&nbsp;",
-            "oPaginate": {
-                "sFirst": "Primero",
-                "sLast": "Último",
-                "sNext": ">",
-                "sPrevious": "<"
-            },
-            "oAria": {
-                "sSortAscending": ": Activar para ordenar la columna de manera ascendente",
-                "sSortDescending": ": Activar para ordenar la columna de manera descendente"
+        language: {
+            sProcessing: '<i class="fa fa-spinner spinner"></i> Cargando...',
+            sLengthMenu: "Mostrar _MENU_",
+            sZeroRecords: "No se encontraron resultados",
+            sEmptyTable: "Ningún dato disponible en esta tabla =(",
+            sInfo: "Mostrando del _START_ al _END_ de _TOTAL_",
+            sInfoEmpty: "Mostrando del 0 al 0 de 0",
+            sInfoFiltered: "(filtrado _MAX_)",
+            sSearch: "Buscar:",
+            oPaginate: {
+                sFirst: "Primero",
+                sLast: "Último",
+                sNext: ">",
+                sPrevious: "<"
             }
         }
     });
-
-    var buttons = new $.fn.dataTable.Buttons(table, {
-        buttons: [
-            {
-                extend: 'excelHtml5',
-                className: 'custom-button'
-            }
-        ]
-    }).container().appendTo($('#buttons'));
 });
 
-function crear_ocurrencia_evento(clase_id) {
-    var button = $('a[data-id="registrar_' + clase_id + '"]');
-    var mensajeEnPantalla = $('#mensaje_en_pantalla'); // Seleccionar el span del mensaje
+async function manejar_evento(clase_id, accion, funcion) {
+    const button = document.querySelector(`#${accion}_${clase_id}`);
+    if (!button) {
+        console.error(`Botón con ID #${accion}_${clase_id} no encontrado`);
+        return;
+    }
+    const mensaje_en_pantalla = document.querySelector('#mensaje_en_pantalla');
 
-    // Deshabilitar el botón inmediatamente para evitar múltiples clics
-    if (button.data('clicked')) {
-        return; // Si el botón ya fue clicado, no hacer nada
+    if (button.dataset.clicked) {
+        return;
     }
 
-    button.data('clicked', true); // Marcar el botón como clicado
-    button.prop('disabled', true); // Deshabilitar el botón
+    button.dataset.clicked = true;
+    button.disabled = true;
 
-    // Cambiar el texto del botón a un loader
-    button.html('<i class="fa fa-spinner spinner"></i> Procesando...');
-    button.removeClass().addClass('text-warning'); // Cambiar estilo para error
+    button.innerHTML = '<i class="fa fa-spinner spinner"></i> Procesando...';
+    button.className = 'text-warning';
 
-    mensajeEnPantalla.html('<i class="fa fa-spinner spinner"></i> Procesando...'); // Mostrar mensaje de éxito
-    mensajeEnPantalla.removeClass().addClass('text-warning'); // Cambiar estilo para error
+    mensaje_en_pantalla.innerHTML = '<i class="fa fa-spinner spinner"></i> Procesando...';
+    mensaje_en_pantalla.className = 'text-warning';
 
-    fetch(method_call + "crear_ocurrencia_evento/" + clase_id, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ clase_id: clase_id })
-    })
-        .then(response => response.json())
-        .then(data => {
-            if (data.success) {
-                mensajeEnPantalla.html(data.message); // Mostrar mensaje de éxito
-                mensajeEnPantalla.removeClass().addClass('text-success'); // Cambiar estilo para éxito
+    try {
+        const response = await fetch(`${methodCall}${funcion}/${clase_id}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ clase_id })
+        });
 
-                var updatedData = data.data;
-                var row = table.row(button.closest('tr'));
+        const data = await response.json();
 
+        if (data.success) {
+            mensaje_en_pantalla.innerHTML = data.message;
+            mensaje_en_pantalla.className = 'text-success';
+
+            const updatedData = data.data;
+            const row = table.row(button.closest('tr'));
+
+            if (row) {
                 row.data({
                     opciones: updatedData.opciones,
                     id: updatedData.id,
@@ -121,141 +103,46 @@ function crear_ocurrencia_evento(clase_id) {
                     cupos: updatedData.cupos,
                 }).draw(false);
             } else {
-                mensajeEnPantalla.html('Error: ' + data.message); // Mostrar mensaje de error
-                mensajeEnPantalla.removeClass().addClass('text-danger'); // Cambiar estilo para error
-                button.html('Registrar'); // Cambiar el texto del botón para reflejar el fallo
+                console.error('Fila no encontrada para actualizar.');
             }
-        })
-        .catch(error => {
-            console.error('Error:', error);
-            mensajeEnPantalla.html('No se pudo crear la clase en TotalPass.'); // Mostrar mensaje de error
-            mensajeEnPantalla.removeClass().addClass('text-danger'); // Cambiar estilo para error
-            button.html('Registrar'); // Cambiar el texto del botón para reflejar el fallo
-        });
+
+        } else {
+            mostrar_error(data.message, button, accion);
+        }
+
+    } catch (error) {
+        console.error('Error:', error);
+        mostrar_error('No se pudo procesar la solicitud en TotalPass.', button, accion);
+    }
+}
+
+function mostrar_error(message, button, accion) {
+    const mensaje_en_pantalla = document.querySelector('#mensaje_en_pantalla');
+    mensaje_en_pantalla.innerHTML = `${message}`;
+    mensaje_en_pantalla.className = 'text-danger';
+    button.innerHTML = capitalize_first_letter(accion);
+    button.disabled = false;
+    delete button.dataset.clicked;
+}
+
+function capitalize_first_letter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function crear_ocurrencia_evento(clase_id) {
+    var boton = 'registrar';
+    var funcion = 'crear_ocurrencia_evento';
+    manejar_evento(clase_id, boton, funcion);
 }
 
 function actualizar_ocurrencia_evento(clase_id) {
-    var button = $('a[data-id="actualizar_' + clase_id + '"]');
-    var mensajeEnPantalla = $('#mensaje_en_pantalla'); // Seleccionar el span del mensaje
-
-    // Deshabilitar el botón inmediatamente para evitar múltiples clics
-    if (button.data('clicked')) {
-        return; // Si el botón ya fue clicado, no hacer nada
-    }
-
-    button.data('clicked', true); // Marcar el botón como clicado
-    button.prop('disabled', true); // Deshabilitar el botón
-
-    // Cambiar el texto del botón a un loader
-    button.html('<i class="fa fa-spinner spinner"></i> Procesando...');
-    button.removeClass().addClass('text-warning'); // Cambiar estilo para error
-
-    mensajeEnPantalla.html('<i class="fa fa-spinner spinner"></i> Procesando...'); // Mostrar mensaje de éxito
-    mensajeEnPantalla.removeClass().addClass('text-warning'); // Cambiar estilo para error
-
-    fetch(method_call + "actualizar_ocurrencia_evento/" + clase_id, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ clase_id: clase_id })
-    })
-        .then(response => response.json())
-        .then(data => {
-            if (data.success) {
-                mensajeEnPantalla.html(data.message); // Mostrar mensaje de éxito
-                mensajeEnPantalla.removeClass().addClass('text-success'); // Cambiar estilo para éxito
-
-                var updatedData = data.data;
-                var row = table.row(button.closest('tr'));
-
-                row.data({
-                    opciones: updatedData.opciones,
-                    id: updatedData.id,
-                    totalpass_event_id: updatedData.totalpass_event_id,
-                    totalpass_eventOccurrenceUuid: updatedData.totalpass_eventOccurrenceUuid,
-                    identificador: updatedData.identificador,
-                    disciplinas_nombre: updatedData.disciplinas_nombre,
-                    dificultad: updatedData.dificultad,
-                    fecha: updatedData.fecha,
-                    horario: updatedData.horario,
-                    instructores_nombre: updatedData.instructores_nombre,
-                    sucursales_locacion: updatedData.sucursales_locacion,
-                    cupos: updatedData.cupos,
-                }).draw(false);
-            } else {
-                mensajeEnPantalla.html(data.message); // Mostrar mensaje de error
-                mensajeEnPantalla.removeClass().addClass('text-danger'); // Cambiar estilo para error
-                button.html('Actualizar'); // Cambiar el texto del botón para reflejar el fallo
-            }
-        })
-        .catch(error => {
-            console.error('* ', error);
-            mensajeEnPantalla.html('No se pudo actualizar la clase en TotalPass.'); // Mostrar mensaje de error
-            mensajeEnPantalla.removeClass().addClass('text-danger'); // Cambiar estilo para error
-            button.html('Actualizar'); // Cambiar el texto del botón para reflejar el fallo
-        });
+    var boton = 'actualizar';
+    var funcion = 'actualizar_ocurrencia_evento';
+    manejar_evento(clase_id, boton, funcion);
 }
 
 function eliminar_ocurrencia_evento(clase_id) {
-    var button = $('a[data-id="cancelar_' + clase_id + '"]');
-    var mensajeEnPantalla = $('#mensaje_en_pantalla'); // Seleccionar el span del mensaje
-
-    // Deshabilitar el botón inmediatamente para evitar múltiples clics
-    if (button.data('clicked')) {
-        return; // Si el botón ya fue clicado, no hacer nada
-    }
-
-    button.data('clicked', true); // Marcar el botón como clicado
-    button.prop('disabled', true); // Deshabilitar el botón
-
-    // Cambiar el texto del botón a un loader
-    button.html('<i class="fa fa-spinner spinner"></i> Procesando...');
-    button.removeClass().addClass('text-warning'); // Cambiar estilo para error
-
-    mensajeEnPantalla.html('<i class="fa fa-spinner spinner"></i> Procesando...'); // Mostrar mensaje de éxito
-    mensajeEnPantalla.removeClass().addClass('text-warning'); // Cambiar estilo para error
-
-    fetch(method_call + "eliminar_ocurrencia_evento/" + clase_id, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ clase_id: clase_id })
-    })
-        .then(response => response.json())
-        .then(data => {
-            if (data.success) {
-                mensajeEnPantalla.html(data.message); // Mostrar mensaje de éxito
-                mensajeEnPantalla.removeClass().addClass('text-success'); // Cambiar estilo para éxito
-
-                var updatedData = data.data;
-                var row = table.row(button.closest('tr'));
-
-                row.data({
-                    opciones: updatedData.opciones,
-                    id: updatedData.id,
-                    totalpass_event_id: updatedData.totalpass_event_id,
-                    totalpass_eventOccurrenceUuid: updatedData.totalpass_eventOccurrenceUuid,
-                    identificador: updatedData.identificador,
-                    disciplinas_nombre: updatedData.disciplinas_nombre,
-                    dificultad: updatedData.dificultad,
-                    fecha: updatedData.fecha,
-                    horario: updatedData.horario,
-                    instructores_nombre: updatedData.instructores_nombre,
-                    sucursales_locacion: updatedData.sucursales_locacion,
-                    cupos: updatedData.cupos,
-                }).draw(false);
-            } else {
-                mensajeEnPantalla.html(data.message); // Mostrar mensaje de error
-                mensajeEnPantalla.removeClass().addClass('text-danger'); // Cambiar estilo para error
-                button.html('Cancelar'); // Cambiar el texto del botón para reflejar el fallo
-            }
-        })
-        .catch(error => {
-            console.error('* ', error);
-            mensajeEnPantalla.html('No se pudo cancelar la clase en TotalPass.'); // Mostrar mensaje de error
-            mensajeEnPantalla.removeClass().addClass('text-danger'); // Cambiar estilo para error
-            button.html('Cancelar'); // Cambiar el texto del botón para reflejar el fallo
-        });
+    var boton = 'cancelar';
+    var funcion = 'eliminar_ocurrencia_evento';
+    manejar_evento(clase_id, boton, funcion);
 }
