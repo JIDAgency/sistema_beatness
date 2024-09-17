69eef54f445aa7ea8958fb1714b98114fe666b13
Eduardo GQ <eduardo@jid.agency>
2024-09-17 10:17:08 -0600

    1.10.1

---
+++ application/views/plataformas/index.php
@@ -0,0 +1,34 @@
+<section class="">
+    <div class="container-fluid p-2">
+        <div class="row match-height">
+
+            <div class="col-xl-12 col-md-12 col-sm-12 text-center pt-2 pb-2">
+                <h2>
+                    Reserva directo en tu plataforma favorita encuéntranos como:
+                    <br>
+                    <br>
+                    <b>“Beatness polanco” y “Beatness Puebla”</b>
+                </h2>
+            </div>
+
+            <div class="col-xl-12 col-md-12 col-sm-12 text-center pt-2 pb-2">
+                <a href="https://www.fitpass.com/mx" target="_blank">
+                    <img class="img-fluid round" src="<?php echo base_url(); ?>almacenamiento/plataformas/boton-fitpass.jpg">
+                </a>
+            </div>
+
+            <div class="col-xl-12 col-md-12 col-sm-12 text-center pt-2 pb-2">
+                <a href="https://totalpass.com/mx" target="_blank">
+                    <img class="img-fluid round" src="<?php echo base_url(); ?>almacenamiento/plataformas/boton-totalpass.jpg">
+                </a>
+            </div>
+
+            <div class="col-xl-12 col-md-12 col-sm-12 text-center pt-2 pb-2">
+                <a href="https://wellhub.com/es-mx" target="_blank">
+                    <img class="img-fluid round" src="<?php echo base_url(); ?>almacenamiento/plataformas/boton-wellhub.jpg">
+                </a>
+            </div>
+
+        </div>
+    </div>
+</section>
